06dd29cadd6236f21234c60c2cabb8af7686072f
Tushar Kapil <tusharkapil@Tushars-MacBook-Air-3.local>
2023-10-29 13:46:20 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
-import dns from "dns";
 
-dns.setDefaultResultOrder("verbatim");
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  server: {
-    host: "localhost",
-    port: "5173",
-  },
 });
