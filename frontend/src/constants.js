82ac04f8c4a61b85e144ed63a166de66d0c2a6bc
Tushar Kapil <tusharkapil@Tushars-MacBook-Air-3.local>
2023-10-29 14:07:21 +0530

    constant

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "";
 export const PRODUCT_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDER_URL = "/api/orders";
